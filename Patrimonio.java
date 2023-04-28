7b2859dae97e76ee275ee8ed5cf9852f983ee8d3
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-04-27 20:49:27 -0400

    primeira classe

---
+++ Patrimonio.java
@@ -0,0 +1,6 @@
+public class Patrimonio{
+  
+  private int nPatrimonio; // numero gerado com random util java
+  private String tpPatrimonio; //tipo de patrimonio (arma, colete, móvel, veiculo)
+  private String estadoPatrimonio; //(novo, velho, com defeito)
+}
