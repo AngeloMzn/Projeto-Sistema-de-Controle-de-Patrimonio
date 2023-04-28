5e4730b34094d87034e5029a47197d922d30899c
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-04-28 15:25:23 -0400

    Update App.java

--- main/App.java
+++ main/App.java
@@ -15,26 +15,39 @@ public class App{
       
       switch(escolha) {
         case 0:
-          System.out.println("Tchau até a próxima !");
+        System.out.println();  
+        System.out.println("Tchau até a próxima !");
+        System.out.println();
           break;
         case 1:
+        System.out.println();
         System.out.println("opção 1");
+        System.out.println();
           break;
 
         case 2:
-          System.out.println("opção 2");
+        System.out.println();  
+        System.out.println("opção 2");
+        System.out.println();
           break;
 
         case 3:
-          System.out.println("opção 3");
+        System.out.println();
+        System.out.println("opção 3");
+        System.out.println();
           break;
 
         case 4:
-          System.out.println("opção 4");
+        System.out.println();  
+        System.out.println("opção 4");
+        System.out.println();
+          
           break;
 
         default:
-          System.out.println("Sua escolha deve ser entre 0 e 4 !");
+        System.out.println();  
+        System.out.println("Sua escolha deve ser entre 0 e 4 !");
+        System.out.println();
       }
     } while(escolha != 0);
   }
