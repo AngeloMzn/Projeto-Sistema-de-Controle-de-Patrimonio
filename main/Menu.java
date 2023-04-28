a5cd590265e6590934ba632cb73e4d7d1645f029
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-04-28 11:12:47 -0400

    Update Menu.java

--- main/Menu.java
+++ main/Menu.java
@@ -1,12 +1,27 @@
 package main;
-public class Menu{
+public class Menu {
 
-  
-  public void listaMenu(){
-    system.out.println()
-   
-  
-  }
+    private int escolha;
+
+    public Menu(){
+
+    }
+
+    public void printaMenu(){
+
+        System.out.println("Olá usuário, o que gostaria de fazer ?");
+        System.out.println("=============opções=============");
+        System.out.println("0 - finaliza o programa");
+        System.out.println("1 - cadastrar patrimonio");
+        System.out.println("2 - movimentar patrimonio");
+        System.out.println("3 - remover partimonio");
+        System.out.println("4 - buscar patrimonio");
+        System.out.println("================================");
+        System.out.println("digite sua escolha: ");
+
+    }
+
+
 
 
 }
