834b2b26c604c40cd62b8cb9213763e8d6aa6d1c
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-04-28 11:12:20 -0400

    Update App.java

--- main/App.java
+++ main/App.java
@@ -6,20 +6,13 @@ public class App{
   public static void main(String[] args){
     int escolha;
     Scanner r = new Scanner(System.in);
-
+    Menu menu = new Menu();
     
     do {
-      System.out.println("Olá usuário, o que gostaria de fazer?");
-      System.out.println("=============opções=============");
-      System.out.println("0 - finaliza o programa");
-      System.out.println("1 - cadastrar patrimonio");
-      System.out.println("2 - movimentar patrimonio");
-      System.out.println("3 - remover partimonio");
-      System.out.println("4 - buscar patrimonio");
-      System.out.println("================================");
-      System.out.println("digite sua escolha: ");
-  
+      
+      menu.printaMenu();
       escolha = r.nextInt();
+      
       switch(escolha) {
         case 0:
           System.out.println("Fim do Programa !");
