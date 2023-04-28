ef69e8a7fd5d9707837d19155ad5a0979b171cea
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-04-27 21:12:01 -0400

    Update Patrimonio.java

--- Patrimonio.java
+++ Patrimonio.java
@@ -3,4 +3,13 @@ public class Patrimonio{
   private int nPatrimonio; // numero gerado com random util java
   private String tpPatrimonio; //tipo de patrimonio (arma, colete, móvel, veiculo)
   private String estadoPatrimonio; //(novo, velho, com defeito)
+
+  // metodo para validar se existe um patrimonio com o numero gerado pelo random na main
+  public boolean existePatrimonio(){
+    
+    
+    
+  }
+
+
 }
